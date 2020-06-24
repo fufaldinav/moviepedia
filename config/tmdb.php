7e7b3b052ac1951d3c0c6ec76268494d09c09c0e
Artem <artem.fufaldin@gmail.com>
2020-06-24 17:33:41 +0500

    Install php-tmdb/laravel package from my repo

---
+++ config/tmdb.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package php-tmdb\laravel
+ * @author Mark Redeman <markredeman@gmail.com>
+ * @copyright (c) 2014, Mark Redeman
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | TMDB API key
+    |--------------------------------------------------------------------------
+    */
+
+    'api_key' => env('TMDB_API_KEY', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | TMDB Client options
+    |--------------------------------------------------------------------------
+    */
+
+    'options' => [
+        'secure' => false,
+        'cache' => [
+            'enabled' => true,
+            'path' => storage_path('tmdb'),
+        ],
+        'log' => [
+            'enabled' => true,
+            'path' => storage_path('logs/tmdb.log'),
+        ],
+
+    ],
+];
